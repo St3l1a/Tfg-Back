c3c3305126eb057d1f8f61537181e53dbefcd1b7
esblanro <esblanro@alumni.uv.es>
2026-06-17 20:34:59 +0200

    Feat: crear model usuario

--- tfgBack/tfg/src/main/java/estrella/tfg/usuario/model/Usuario.java
+++ tfgBack/tfg/src/main/java/estrella/tfg/usuario/model/Usuario.java
@@ -1,4 +1,45 @@
 package estrella.tfg.usuario.model;
 
+import jakarta.persistence.*;
+import org.hibernate.mapping.PrimaryKey;
+import org.springframework.context.annotation.Primary;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "correo", nullable = false)
+    private String correo;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "nombre", nullable = false)
+    private String telefono;
+
+    @Column(name = "profesional")
+    private boolean profesional;
+
+    @Column(name = "contrasena", nullable = false)
+    private String contrasena;
+
+    @Column(name = "edad")
+    private Integer edad;
+
+    @Column(name = "sexo")
+    private Character sexo;
+
+    @Column(name = "pareja")
+    private String pareja;
+
+    @Column(name = "familia")
+    private String familia;
+
+    @Column(name = "situacionEconomica")
+    private String situacionEconomica;
+
 }
