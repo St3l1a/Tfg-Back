7ccfd57b8c159cf54e47b83aa362b4683f2116ea
esblanro <esblanro@alumni.uv.es>
2026-06-20 20:20:52 +0200

    Feat: crear controller

--- tfgBack/tfg/src/main/java/estrella/tfg/usuario/UsuarioController.java
+++ tfgBack/tfg/src/main/java/estrella/tfg/usuario/UsuarioController.java
@@ -1,4 +1,51 @@
 package estrella.tfg.usuario;
 
+import estrella.tfg.usuario.model.Usuario;
+import estrella.tfg.usuario.model.UsuarioDto;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RequestMapping(value = "/usuario")
+@RestController
+@CrossOrigin(origins = "*")
 public class UsuarioController {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    ModelMapper mapper;
+
+
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<UsuarioDto> findAll() {
+        List<Usuario> usuarios = this.usuarioService.findAll();
+
+        return usuarios.stream().map(e -> mapper.map(e, UsuarioDto.class)).collect(Collectors.toList());
+    }
+
+
+    @RequestMapping(path = { "", "/{id}" }, method = RequestMethod.PUT)
+    public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody UsuarioDto dto) {
+
+        this.usuarioService.save(id, dto);
+    }
+
+    /**
+     * Método para borrar una {@link Usuario}
+     *
+     * @param id PK de la entidad
+     */
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") Long id) throws Exception {
+
+        this.usuarioService.delete(id);
+    }
+
 }
